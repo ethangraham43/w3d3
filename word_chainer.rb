3953ebbc7084318ebe2120f04cdb5c229e3b14f5
Ethan Graham <ethangraham43@gmail.com>
2022-09-28 16:41:35 -0400

    wordchainer phase 2a

--- word_chainer.rb
+++ word_chainer.rb
@@ -21,10 +21,25 @@ class WordChainer
     def run(source,target)
         @all_seen_words = [source]
         @current_words = [source]
-        
+        while @current_words.length > 0
+            new_current_words = []
+            @current_words.each do |current_word|
+                adj_words = adjacent_words(current_word)
+                adj_words.each do |word|
+                    if !@all_seen_words.include?(word)
+                        new_current_words << word
+                        @all_seen_words << word
+                    end
+                end
+            end
+            p new_current_words
+            @current_words = new_current_words
+        end
+
+        @all_seen_words.include?(target)
     end
 
 end
 
-dict = WordChainer.new(['can','cats','bat','man','men']) #add 'cat' for harder case
-p dict.adjacent_words('cat')
+dict = WordChainer.new(['cat','can','cats','bat','man']) #add 'cat' for harder case
+p dict.run('cat', 'men')
